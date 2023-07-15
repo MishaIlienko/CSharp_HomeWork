8f15bde4eeb92212605f5a7ee56b38334c569b8f
Misha <mnwahr@gmail.com>
2023-07-15 16:08:38 +0200

    Finish_004

---
+++ Task004_AllNumbersFrom1ToN/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число!");
+string input = Console.ReadLine();
+int num = Convert.ToInt32(input);
+int x = 2;
+while (x <= num)
+{
+    Console.Write($" {x}");
+    x+=2;
+}
