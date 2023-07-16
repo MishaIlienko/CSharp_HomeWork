5c21dee0a0a8e025f04006704fce8f7f125dfbc0
Misha <mnwahr@gmail.com>
2023-07-16 16:00:55 +0200

    Update001

--- Task001_MaxOfTwoNumbers/Program.cs
+++ Task001_MaxOfTwoNumbers/Program.cs
@@ -6,10 +6,6 @@ int num = Convert.ToInt32(input);
 Console.WriteLine("Введите второе число!");
 string input1 = Console.ReadLine();
 int num1 = Convert.ToInt32(input1);
-if (num == num1)
-{
-    Console.WriteLine("Оба числа равны!");
-}
 if (num > num1)
 {
 Console.WriteLine($"Max = {num}");
@@ -20,3 +16,8 @@ if (num1 > num)
 Console.WriteLine($"Max = {num1}");
 Console.WriteLine($"Min = {num}");
 }
+else
+{
+    Console.WriteLine("Оба числа равны!");
+}
+
