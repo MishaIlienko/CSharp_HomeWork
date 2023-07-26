446debe922cf5901eddb1ecc9a091bb586b0a5f7
Misha <mnwahr@gmail.com>
2023-07-26 22:50:17 +0200

    Finale205

---
+++ Task205_SecondDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. 
+// Не использовать строки для расчета.
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+bool Validate3sign(int num)
+{
+    if (num >= 100 && num < 1000)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Число НЕ является трехзначным!");
+    return false;
+}
+int number = ReadInt("Введите число!");
+int SecondDigit = number / 10 % 10;
+if (Validate3sign(number))
+{
+    System.Console.WriteLine(SecondDigit);
+}
