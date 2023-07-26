967a382b49899f8d7ff94023ce9fb4aa5cbe076a
Misha <mnwahr@gmail.com>
2023-07-26 22:48:45 +0200

    Finale206

---
+++ Task206_ThirdDigit/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, 
+//что третьей цифры нет. 
+//Не использовать строки для расчета.
+
+int ReadInt(string message)
+{
+    System.Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+bool Validate3sign (int num)
+{
+    if (num >=100 && num < 1000)
+    {
+        return true;
+    }
+    return false;
+}
+int number = ReadInt("Веведите число!");
+int rest10 = 0;
+int thirdDigit = 0;
+if (number > 100)
+{
+    while (number > 0)
+    {
+        rest10 = number % 10;
+        if (Validate3sign(number))
+        {
+            thirdDigit = rest10;
+        }
+        number /= 10;
+    }
+    System.Console.WriteLine($"Третья цифра числа -> {thirdDigit}");
+}
+else
+{
+    System.Console.WriteLine($"В числе {number} - третьей цифры нет!");
+}
+
